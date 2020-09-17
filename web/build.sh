97569b1f6c226153ba66c66ed1cc031fd3eec5bd
Wael Jammal <wael.jammal@askmti.com>
2020-09-17 20:04:38 +0100

    wip

--- web/build.sh
+++ web/build.sh
@@ -5,6 +5,7 @@ export NG_CLI_ANALYTICS=ci
 
 echo '+++ Running npm install'
 npm install
+npm install -g @angular/cli@~${NG_CLI_VERSION:9.1.5}
 echo '+++ Running npm build'
 npm run build -- --output-path=./dist
 echo '+++ Generating package'
