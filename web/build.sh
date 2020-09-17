3710c97c699e1ef5adfac57e0a8cd3a64be5bcec
Wael Jammal <wael.jammal@askmti.com>
2020-09-17 22:52:28 +0100

    wip

--- web/build.sh
+++ web/build.sh
@@ -3,10 +3,19 @@ set -euo pipefail
 
 export NG_CLI_ANALYTICS=ci
 
+BASE_DIRECTORY=`pwd`
+PACKAGE_FILENAME="$PROJECT.tar.gz"
+TMP_DIRECTORY=$BASE_DIRECTORY/tmp
+
+# Ensure the tmp release directory exists
+rm -rf $TMP_DIRECTORY
+mkdir -p $TMP_DIRECTORY
+
 echo '+++ Running npm install'
 npm install
 npm install -g @angular/cli@~${NG_CLI_VERSION:-9.1.5}
 echo '+++ Running npm build'
-npm run build -- --output-path=./dist
-echo '+++ Generating package'
-ls -la ./dist
+npm run build -- --output-path="$TMP_DIRECTORY"
+echo '+++ Generating artifact'
+tar -zcvf "./dist/$PACKAGE_FILENAME" "$TMP_DIRECTORY"
+
