66274319026b965bf445551bcc8f58c6175ef547
Wael Jammal <wael.jammal@askmti.com>
2020-09-18 14:43:13 +0100

    wip

--- web/build-docker-image.sh
+++ web/build-docker-image.sh
@@ -22,6 +22,6 @@ mkdir -p pkg/app
 #tar -czxf ./${PACKAGE_NAME}.tar.gz --directory ./pkg/app
 aws s3 cp s3://mti-ci-artifacts/85/dist/ptg-mobile-1.4.85.tar.gz ptg-mobile-1.4.85.tar.gz
 tar zxf ./ptg-mobile-1.4.85.tar.gz --directory ./pkg/app
-cp ./web/Dockerfile ./pkg/Dockerfile
+cp .ci/web/Dockerfile ./pkg/Dockerfile
 docker build --tag ${IMAGE_TAG} ./pkg
 docker push ${IMAGE_TAG}
