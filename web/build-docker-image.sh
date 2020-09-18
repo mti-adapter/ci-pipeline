a52bc5b8bd4c7889a298bdb62c05f19c447df398
Wael Jammal <wael.jammal@askmti.com>
2020-09-18 19:10:49 +0100

    wip

--- web/build-docker-image.sh
+++ web/build-docker-image.sh
@@ -24,6 +24,7 @@ docker login --username=${NEXUS_LOGIN_USER} --password=${NEXUS_LOGIN_PASSWORD} $
 
 # Download the package
 buildkite-agent artifact download "dist/${PACKAGE_NAME}.tar.gz" .
+ls -la .
 tar zxf ./${PACKAGE_NAME}.tar.gz --directory ./pkg/app
 
 #aws s3 cp s3://mti-ci-artifacts/85/dist/ptg-mobile-1.4.85.tar.gz ptg-mobile-1.4.85.tar.gz
