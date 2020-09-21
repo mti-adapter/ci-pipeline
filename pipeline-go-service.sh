eeabecd306bc2b1e551d279a8463924cb8e743a9
Wael Jammal <wael.jammal@askmti.com>
2020-09-21 13:22:11 +0100

    wip

---
+++ pipeline-go-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+echo '+++ Merging go service pipeline steps'
+
+
+merge_steps_yaml() {
+    value=$(<.ci/pipeline-go-service.yml)
+    echo "$value"
+}
+
+# Set environments, process version upgrades etc.
+./.ci/shared/update-environment.sh
+
+merge_steps_yaml | buildkite-agent pipeline upload
