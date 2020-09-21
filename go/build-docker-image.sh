05f5af99eaee7acae3cd16a76b7724032a6492a5
Wael Jammal <wael.jammal@askmti.com>
2020-09-21 17:47:49 +0100

    wip

--- go/build-docker-image.sh
+++ go/build-docker-image.sh
@@ -27,7 +27,7 @@ docker login --username=${NEXUS_LOGIN_USER} --password=${NEXUS_LOGIN_PASSWORD} $
 
 # Download the package
 aws s3 cp s3://mti-ci-artifacts/${PROJECT}/${FULL_VERSION}/${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar.gz
-tar zxf ./${PACKAGE_NAME}.tar.gz --directory ./pkg
+tar xvf ./${PACKAGE_NAME}.tar.gz -C ./pkg
 ls -la ./pkg
 mv ./pkg/${PACKAGE_NAME} ./pkg/app
 ls -la ./pkg
